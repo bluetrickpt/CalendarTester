1f966efa8a37335d43bf92e2ad8381e563a213e6
bluetrickpt <ricardo.s.c.mendes@gmail.com>
2017-07-17 23:21:05 +0100

    v1.0 of CalendarTester. Readme with a small explanation

---
+++ README.md
@@ -0,0 +1,44 @@
+# Calendar Tester v1.0
+
+## Description
+
+This small app serves as an example of how one can retrieve the events from the installed calendars.
+
+Currently retrieves, for each event:
+            CalendarContract.Events._ID,
+            CalendarContract.Events.CALENDAR_ID,
+            CalendarContract.Events.EVENT_LOCATION,
+            CalendarContract.Events.DTSTART,
+            CalendarContract.Events.DTEND,
+            CalendarContract.Events.EVENT_TIMEZONE,
+            CalendarContract.Events.EVENT_END_TIMEZONE,
+            CalendarContract.Events.DURATION,
+            CalendarContract.Events.ALL_DAY,
+            CalendarContract.Events.RRULE,
+            CalendarContract.Events.RDATE,
+            CalendarContract.Events.AVAILABILITY,
+            CalendarContract.Events.GUESTS_CAN_MODIFY,
+            CalendarContract.Events.GUESTS_CAN_INVITE_OTHERS,
+            CalendarContract.Events.GUESTS_CAN_SEE_GUESTS
+This is not all the information one can retrieve from the event (this was not the objective).
+
+This application requires READ_CALENDAR permission and it will ask for this permission at runtime if device sdk >= android Marshmallow (api lvl 23), or at install time for older devices.
+
+## Small output example
+
+            Event 1 of 1
+            _id: 1
+            calendar_id: 4
+            eventLocation: Office
+            dtstart: 1500310800000
+            dtend: null
+            eventTimezone: Europe/Lisbon
+            eventEndTimezone: null
+            duration: P3600S
+            allDay: 0
+            rrule: FREQ=WEEKLY;WKST=SU
+            rdate: null
+            availability: 0
+            guestsCanModify: 0
+            guestsCanInviteOthers: 1
+            guestsCanSeeGuests: 1
---
+++ app/src/main/java/com/example/rmendes/calendartester/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.rmendes.calendartester;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.CalendarContract;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+
+    private static final String CALENDAR_PERMISSION = Manifest.permission.READ_CALENDAR;
+    final private int REQUEST_PERMISSION_CODE = 123;
+
+    public static final String[] FIELDS = { //This list is not a complete set of event information
+            CalendarContract.Events._ID,
+            CalendarContract.Events.CALENDAR_ID,
+            CalendarContract.Events.EVENT_LOCATION,
+            CalendarContract.Events.DTSTART,
+            CalendarContract.Events.DTEND,
+            CalendarContract.Events.EVENT_TIMEZONE,
+            CalendarContract.Events.EVENT_END_TIMEZONE,
+            CalendarContract.Events.DURATION,
+            CalendarContract.Events.ALL_DAY,
+            CalendarContract.Events.RRULE,
+            CalendarContract.Events.RDATE,
+            CalendarContract.Events.AVAILABILITY,
+            CalendarContract.Events.GUESTS_CAN_MODIFY,
+            CalendarContract.Events.GUESTS_CAN_INVITE_OTHERS,
+            CalendarContract.Events.GUESTS_CAN_SEE_GUESTS
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button getLogBtn = (Button) findViewById(R.id.get_log_btn);
+        getLogBtn.setOnClickListener(new View.OnClickListener() { //On button click, runs our permission logger (runPermissionLogger())
+            @Override
+            public void onClick(View view) {
+                runCalendarLogger();
+            }
+        });
+    }
+
+    private void runCalendarLogger() {
+        Context context = getApplicationContext();
+        ContentResolver contentResolver = context.getContentResolver();
+        Uri uri = CalendarContract.Events.CONTENT_URI;
+
+        if(hasCalendarPermission(context)) {
+            Cursor eventCursor = contentResolver.query(uri, FIELDS, null, null, null);
+            if(eventCursor!=null) {
+                int eventCount = eventCursor.getCount();
+                if(eventCount > 0) { //there's at least one event
+                    while(eventCursor.moveToNext()) {
+                        Log.d(TAG, "Event " + (eventCursor.getPosition()+1) + " of " + eventCount);
+                        for (String field : FIELDS) {
+                            Log.d(TAG, field + ": " + eventCursor.getString(eventCursor.getColumnIndex(field)));
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "No calendar events found.");
+                }
+            } else {
+                Log.w(TAG, "Failed to retrieve calendars");
+            }
+            eventCursor.close();
+        } else {
+            Log.w(TAG, "No calendar access permission");
+        }
+    }
+
+    private boolean hasCalendarPermission(Context context) {
+
+        if(!hasRuntimePermissions())//If it's before Marshmallow, it is granted at install time
+            return true;
+
+        //Android M and over have runtime permissions
+        int result = context.checkSelfPermission(CALENDAR_PERMISSION);
+        if (result == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[] {CALENDAR_PERMISSION}, REQUEST_PERMISSION_CODE);
+        }
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    this.runCalendarLogger();
+                } else {
+                    // Permission Denied
+                    if(hasRuntimePermissions() && shouldShowRequestPermissionRationale(CALENDAR_PERMISSION)) {
+                        presentPermissionRequestDialog();
+                    }
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private boolean hasRuntimePermissions() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
+    private void presentPermissionRequestDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.permission_dialog_title)
+                .setMessage(R.string.dialog_message)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // yes
+                        if(hasRuntimePermissions())
+                            requestPermissions(new String[] {CALENDAR_PERMISSION}, REQUEST_PERMISSION_CODE);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, R.string.no_permission_alert, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
